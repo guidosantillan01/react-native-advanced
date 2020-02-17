22ad3ec90ef3def2a7b3fcb22709a4f84b52ec9c
Guido Santillan <guidosantillan01@gmail.com>
2020-02-17 11:44:04 +0100

    Add comments

--- swipe/src/Ball.js
+++ swipe/src/Ball.js
@@ -3,7 +3,11 @@ import { View, Animated } from 'react-native';
 
 class Ball extends Component {
   componentWillMount() {
+    // 1. Where is the element right now?
     this.position = new Animated.ValueXY(0, 0);
+
+    // 2. Where is the element moving to?
+    // spring -> Move the position
     Animated.spring(this.position, {
       toValue: { x: 200, y: 500 },
     }).start();
@@ -11,6 +15,8 @@ class Ball extends Component {
 
   render() {
     return (
+      // 3. Which element are we moving?
+      // Animated.View element we are moving
       <Animated.View style={this.position.getLayout()}>
         <View style={styles.ball} />
       </Animated.View>
